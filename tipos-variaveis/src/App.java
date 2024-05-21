5fceca5bb961cdc2a0c9445fa3905c966a4968db
wendellCR73 <wwendell30@gmail.com>
2024-05-21 01:56:11 -0300

    Tentativa de exemplos primitivos

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        Todo valor que queremos deixar FIXO, deve ser escrito em CAIXA ALTA e com "final" escrito antes do tipo da variavel;
+
+        ex: final double VALOR_DE_PI = 3.14;
+
+        System.out.println("VALOR_DE_PI");
+
+
+    }
+}
